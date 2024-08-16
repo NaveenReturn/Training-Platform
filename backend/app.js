380be16fded5419cb6fbb6916a595a1a237e08b2
NaveenReturn <nknaveen8075@gmail.com>
2024-08-16 15:25:40 +0530

    backend vercelupdate

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const path = require("path");
+const bodyparser = require("body-parser")
 const cookieParser = require("cookie-parser");
 const app = express();
-app.use(express.json());
+app.use(bodyparser.json());
 app.use(cookieParser());
 const cors = require("cors");
 require("dotenv").config({path:path.join(__dirname,"/config.env")});
