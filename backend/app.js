0db200d7606c4ff632258974a0b206d4e06b8a7f
NaveenReturn <nknaveen8075@gmail.com>
2024-08-16 16:15:01 +0530

    backend vercelupdate

--- backend/app.js
+++ backend/app.js
@@ -1,24 +1,34 @@
 const express = require("express");
 const path = require("path");
-const bodyparser = require("body-parser")
-const cookieParser = require("cookie-parser");
-const app = express();
-app.use(bodyparser.json());
-app.use(cookieParser());
-const cors = require("cors");
+const bodyparser = require("body-parser");
 require("dotenv").config({path:path.join(__dirname,"/config.env")});
+const Users = require("./module/autherSchema")
+const app = express();
+
+app.use(bodyparser.json());
+
+const cors = require("cors");
 // Router api
 const database = require("./database");
-const Usersrouter = require("./routers/autherRouter");
-const Course = require("./routers/courseRouter");
+
 const corsOptions={
     origin:process.env.APPLICATION_URL,
     methods:"GET,POST,PATCH,PUT,DELETE"
 }
 app.use(cors(corsOptions))
 
-app.use("/api/v1",Usersrouter);
-app.use("/api/v1/course",Course);
+app.post("/api/v1/register",async (req,res)=>{
+     
+    try{
+
+        const user = await Users.create(req.body);
+
+        return res.status(200).send("success")
+    }catch(err){
+       return res.status(500).send("server error")
+    }
+})
+
 const PORT = process.env.PORT || 8000;
 app.listen(PORT,()=>{
     console.log(`server is start ${PORT}`)
